6a7e4d69455dee88796eabd085c930d72239dbef
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-17 18:25:35 +0000

    added error handling for empty string and eslint/formatted express_server.js

--- express_server.js
+++ express_server.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port setup
+var PORT = process.env.PORT || 8080;
 var cookieParser = require('cookie-parser');
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.set("view engine", "ejs");
+
+
+// Generating random string
+function generateRandomString(){
+  return Math.random().toString(20).slice(8);
+}
+
+
 
 // Initial database for urls and users
 const urlDatabase = {
@@ -14,44 +22,48 @@ const urlDatabase = {
 };
 
 const users = {
-  "userRandomID":{
+  "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
-  "user2RandomID":{
+  "user2RandomID": {
     id: "user2RandomID",
-    email:"user2@example.com",
-    password:"dishwasher-funk"
+    email: "user2@example.com",
+    password: "dishwasher-funk"
   }
-}
+};
 
 
 // Login and Logout
-app.post("/login",(request,response) => {
-response.cookie('username',request.body.username);
-response.redirect("/urls");
+app.post("/login", (request, response) => {
+  response.cookie('username', request.body.username);
+  response.redirect("/urls");
 });
 
-app.post("/logout", (request,response) => {
-response.clearCookie('username');
-response.redirect("/urls");
+app.post("/logout", (request, response) => {
+  response.clearCookie('username');
+  response.redirect("/urls");
 });
 
 // User Registration
-app.get("/register", (request,response) =>{
+app.get("/register", (request, response) =>{
   response.render("register");
 });
-app.post("/register",(request,response) =>{
+app.post("/register", (request, response) =>{
   let userEmail = request.body.email;
   let userPassword = request.body.password;
   let userID = generateRandomString();
+  if (userEmail === "" || userPassword === "" ){
+    response.status(400);
+    response.send('400 - Bad request');
+  }
   users[userID] = {
     id: userID,
     email: userEmail,
     password: userPassword
-  }
-  response.cookie('user_id',userID);
+  };
+  response.cookie('user_id', userID);
   response.redirect("/urls");
 });
 
@@ -60,20 +72,20 @@ app.get("/", (request, response) => {
   response.end("Hello!");
 });
 
-app.get("/urls", (request,response) => {
-  response.render("urls_index",{
+app.get("/urls", (request, response) => {
+  response.render("urls_index", {
     urls: urlDatabase,
     username: request.cookies["username"]
   });
-})
+});
 
 app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase); // .json ensures proper display on the page
+  response.json(urlDatabase);
 });
 
 
 // Individual id url page and redirection
-app.get("/urls/:id", (request,response) => {
+app.get("/urls/:id", (request, response) => {
   let longURL = urlDatabase[request.params.id];
   response.render("urls_show", {
     shortURL: request.params.id,
@@ -81,37 +93,37 @@ app.get("/urls/:id", (request,response) => {
     username: request.cookies["username"]
   });
 });
-app.get("/u/:shortURL",(request,response) => {
+app.get("/u/:shortURL", (request, response) => {
   let shortURL = request.params.shortURL;
   response.redirect(urlDatabase[shortURL]);
-})
+});
 
 
 
 // Creating, modifying, and deleting URLs
-app.get("/urls/new",(request,response) => {
+app.get("/urls/new", (request, response) => {
   response.render("urls_new", {
     username: request.cookies["username"]
   });
 });
 
-app.post("/urls", (request,response) => {
+app.post("/urls", (request, response) => {
   let randomString = generateRandomString();
-  urlDatabase[randomString] = request.body.longURL
-  response.redirect('/urls/'+randomString);
+  urlDatabase[randomString] = request.body.longURL;
+  response.redirect('/urls/' + randomString);
 });
 
-app.post("/urls/:id/delete",(request,response) => {
-var shortURL = request.params.id;
-delete urlDatabase[shortURL];
-response.redirect('/urls/');
+app.post("/urls/:id/delete", (request, response) => {
+  var shortURL = request.params.id;
+  delete urlDatabase[shortURL];
+  response.redirect('/urls/');
 });
 
-app.post("/urls/:id/update",(request,response) => {
-let updatedURL = request.body.updatedURL;
-let id = request.params.id;
-urlDatabase[id] = updatedURL;
-response.redirect('/urls');
+app.post("/urls/:id/update", (request, response) => {
+  let updatedURL = request.body.updatedURL;
+  let id = request.params.id;
+  urlDatabase[id] = updatedURL;
+  response.redirect('/urls');
 });
 
 
@@ -125,10 +137,3 @@ app.listen(PORT, () => {
 
 
 
-// Generating random string
-
-function generateRandomString(){
-
-return Math.random().toString(20).slice(8);
-
-}
