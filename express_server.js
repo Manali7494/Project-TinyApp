661656bb6502ee904bbb1e8846dcc3ace9932157
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-17 17:56:02 +0000

    reorganized server code with comments

--- express_server.js
+++ express_server.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
-
 app.set("view engine", "ejs");
 
+// Initial database for urls and users
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9s5m5xK': 'http://www.google.com'
@@ -27,7 +27,35 @@ const users = {
 }
 
 
-// Identifies path and action
+// Login and Logout
+app.post("/login",(request,response) => {
+response.cookie('username',request.body.username);
+response.redirect("/urls");
+});
+
+app.post("/logout", (request,response) => {
+response.clearCookie('username');
+response.redirect("/urls");
+});
+
+// User Registration
+app.get("/register", (request,response) =>{
+  response.render("register");
+});
+app.post("/register",(request,response) =>{
+  let userEmail = request.body.email;
+  let userPassword = request.body.password;
+  let userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: userEmail,
+    password: userPassword
+  }
+  response.cookie('user_id',userID);
+  response.redirect("/urls");
+});
+
+// Welcome and URL lists page
 app.get("/", (request, response) => {
   response.end("Hello!");
 });
@@ -43,12 +71,8 @@ app.get("/urls.json", (request, response) => {
   response.json(urlDatabase); // .json ensures proper display on the page
 });
 
-app.get("/urls/new",(request,response) => {
-  response.render("urls_new", {
-    username: request.cookies["username"]
-  });
-});
 
+// Individual id url page and redirection
 app.get("/urls/:id", (request,response) => {
   let longURL = urlDatabase[request.params.id];
   response.render("urls_show", {
@@ -62,14 +86,19 @@ app.get("/u/:shortURL",(request,response) => {
   response.redirect(urlDatabase[shortURL]);
 })
 
+
+
+// Creating, modifying, and deleting URLs
+app.get("/urls/new",(request,response) => {
+  response.render("urls_new", {
+    username: request.cookies["username"]
+  });
+});
+
 app.post("/urls", (request,response) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = request.body.longURL
   response.redirect('/urls/'+randomString);
-});
-
-app.get("/register", (request,response) =>{
-  response.render("register");
 });
 
 app.post("/urls/:id/delete",(request,response) => {
@@ -77,7 +106,6 @@ var shortURL = request.params.id;
 delete urlDatabase[shortURL];
 response.redirect('/urls/');
 });
-
 
 app.post("/urls/:id/update",(request,response) => {
 let updatedURL = request.body.updatedURL;
@@ -87,33 +115,12 @@ response.redirect('/urls');
 });
 
 
-app.post("/register",(request,response) =>{
-  let userEmail = request.body.email;
-  let userPassword = request.body.password;
-  let userID = generateRandomString();
-  users[userID] = {
-    id: userID,
-    email: userEmail,
-    password: userPassword
-  }
-  response.cookie('user_id',userID);
-  response.redirect("/urls");
-});
 // Listening to port
 app.listen(PORT, () => {
   console.log(`Example app is listening to port ${PORT}!`);
 });
 
-app.post("/login",(request,response) => {
-response.cookie('username',request.body.username);
-response.redirect("/urls");
-});
 
-
-app.post("/logout", (request,response) => {
-response.clearCookie('username');
-response.redirect("/urls");
-});
 
 
 
